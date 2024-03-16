b80c4cb6ce5a5c12f19ab84f6b9807703a9f7928
Abdul Barik <abdulbarik1997m@gmail.com>
2024-03-17 03:35:58 +0600

    Login & Signup Page create

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import "../styles/App.css";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  return <Layout>{<Home />}</Layout>;
+  return (
+    <Layout>
+      <Home />
+      <Signup />
+      <Login />
+    </Layout>
+  );
 }
 
 export default App;
