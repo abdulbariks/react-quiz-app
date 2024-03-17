7fe49c67d38fed48b92471b42c5160fc0a66b266
Abdul Barik <abdulbarik1997m@gmail.com>
2024-03-18 03:44:40 +0600

    create Quiz page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
     <Layout>
-      <Home />
+      {/* <Home />
       <Signup />
-      <Login />
+      <Login /> */}
+      <Quiz />
     </Layout>
   );
 }
