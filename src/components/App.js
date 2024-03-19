9931a14c9e153f1e570143c6344fc20dbba52afc
Abdul Barik <abdulbarik1997m@gmail.com>
2024-03-20 02:44:33 +0600

    react-router-dom added with all pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Quiz from "./pages/Quiz";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-// import Signup from "./pages/Signup";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login />
-      <Quiz /> */}
-      <Result />
-    </Layout>
+    // <Layout>
+    //   <Home />
+    //   <Signup />
+    //   <Login />
+    //   <Quiz />
+    //   <Result />
+    // </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="result" element={<Result />} />
+          {/* <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/quiz" component={Quiz} /> */}
+          {/* <Route exact path="/result" component={Result} /> */}
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
